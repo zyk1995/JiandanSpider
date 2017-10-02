211b0ba7b87aba62edae9a071fb1bb68ad59b85a
zyk1995 <1251134350@qq.com>
2017-10-02 22:11:40 +0800

    修改描述

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="JiandanSpider",
         version='1.0',
-        description='A simple example',
+        description='煎蛋网妹子图爬虫',
         author='ZYK',
         author_email='1251134350@qq.com',
         url="https://jandan.net/ooxx",
